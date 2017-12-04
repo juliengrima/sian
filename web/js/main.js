03f88ef7534eb49cc69837f9d58c36ce0fc3ecdd
juliengrima <gogrinformatique@gmail.com>
2017-12-04 15:30:29 +0100

    elevate zoom lens

--- web/js/main.js
+++ web/js/main.js
@@ -54,13 +54,5 @@ function modal() {
     $('.modal').modal();
 }
 
-function zoom1(){
-    $("#zoom_01").elevateZoom({
-        zoomType				: "lens",
-        lensShape : "round",
-        lensSize    : 200
-    });
-}
-
 
 
