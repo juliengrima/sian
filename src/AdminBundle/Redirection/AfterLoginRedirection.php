74b73693c3612d72474adc41b9e41e3d4327b25e
juliengrima <gogrinformatique@gmail.com>
2017-12-09 17:44:03 +0100

    bug security

--- src/AdminBundle/Redirection/AfterLoginRedirection.php
+++ src/AdminBundle/Redirection/AfterLoginRedirection.php
@@ -37,7 +37,7 @@ class AfterLoginRedirection implements AuthenticationSuccessHandlerInterface
             return $role->getRole();
         }, $roles);
         // If is a admin or super admin we redirect to the backoffice area
-        if (in_array('ROLE_ADMIN', $rolesTab, true) || in_array('ROLE_SUPER_ADMIN', $rolesTab, true))
+        if (in_array('ROLE_SUPER_ADMIN', $rolesTab, true))
             $redirection = new RedirectResponse($this->router->generate('fos_user_security_login'));
         // otherwise, if is a commercial user we redirect to the crm area
         else
